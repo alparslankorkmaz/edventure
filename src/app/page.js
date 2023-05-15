a8da3c88c7e75c059b009065fa30be3e06a6f193
Alparslan Korkmaz <95434744+alparslankorkmaz@users.noreply.github.com>
2023-05-15 16:33:15 +0100

    component name changed to pascal case

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 import Content from "@/components/Content";
-import Howwework from "@/components/Howwework";
+import HowWeWork from "@/components/HowWeWork";
 
 export default function Home() {
   return (
@@ -9,7 +9,7 @@ export default function Home() {
       <Header />
       <Hero />
       <Content />
-      <Howwework />
+      <HowWeWork />
     </main>
   );
 }
