00c89a7955fd045551389ef0d6b7ce9816e4cec2
Alparslan Korkmaz <95434744+alparslankorkmaz@users.noreply.github.com>
2023-05-17 09:20:40 +0100

    footer style changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,29 @@
 export default function Footer() {
   return (
     <>
-      <div className="relative mt-20">
-        <div className="z-0">
-          {" "}
-          <img src="/images/hero-pattern-footer.webp" alt="pattern" />
-        </div>
-        <div className="footer bg-[#2D2D2D] rounded-t-3xl p-6 grid grid-cols-2 md:gap-48 items-center absolute z-10 top-10">
-          <div className="">
+      <div>
+        <div className="relative mt-20">
+          <div className="z-0">
+            {" "}
             <img
-              src="/images/logo.png"
-              alt="Edventure Logo"
-              className="w-48 mb-11 m-auto"
+              src="/images/hero-pattern-footer.webp"
+              alt="pattern"
+              className=""
             />
           </div>
-          <div className="">
-            <p className="text-white">
-              EdVenture Marketing Agency | © 2023 All Rights Reserved
-            </p>{" "}
+          <div className="footer bottom-0 w-full bg-[#2D2D2D] rounded-t-3xl p-6 grid grid-cols-2 md:gap-44 items-center absolute z-10 top-10">
+            <div className="flex m-auto">
+              <img
+                src="/images/logo.png"
+                alt="Edventure Logo"
+                className="w-48"
+              />
+            </div>
+            <div className="">
+              <p className="text-white text-right">
+                EdVenture Marketing Agency | © 2023 All Rights Reserved
+              </p>{" "}
+            </div>
           </div>
         </div>
       </div>
